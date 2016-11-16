233b110c27bfc0c1cd3e661fe4fcb392fc167b2e
dackolko <cherepahin-artem@ya.ru>
2016-11-16 23:51:41 +0800

    correct

--- uploads/init.bash
+++
@@ -1,8 +0,0 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
-sudo gunicorn --bind 0.0.0.0:8080 hello &
-sudo /etc/init.d/mysql start
-
-
