a4e4b281f497cdb1633e0237438544a39462b1d0
dackolko <cherepahin-artem@ya.ru>
2016-11-17 00:17:36 +0800

    Vot tak

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo rm -rf /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn --bind 0.0.0.0:8080 hello &
-sudo /etc/init.d/mysql start
 
 
