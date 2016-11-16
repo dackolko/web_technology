1eea2c84d342d87332181747d2790d0dd3f05502
dackolko <cherepahin-artem@ya.ru>
2016-11-16 23:37:56 +0800

    Скорректировано 1.8

--- etc/hello.py
+++ etc/hello.py
@@ -1,3 +1,4 @@
-#pythonpath = "/home/box/web/"
-bind = "0.0.0.0:8080"
+bind = '0.0.0.0:8080'
+logfile = '../gunicorn.log'
+workers = 1
 
--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
-def application(environ, start_response):
-	args = environ["QUERY_STRING"]
-	args = args.split('&')
-	body = '\n'.join(args)
-	status = "200 OK"
-	headers = [("Content-Type", "text/plain")]
-	start_response(status, headers)
-	return [body]
+def hello_app(env, start_response):
+    body = [p for p in env['QUERY_STRING'].split('&')]
+    body = '\n'.join(body)
+    status = '200 OK'
+    response_headers = [('Content-Type', 'text/plain'),
+                        ('Content-Length', str(len(body)))
+    ]
+
+    start_response(status, response_headers)
+    return [ body ]
